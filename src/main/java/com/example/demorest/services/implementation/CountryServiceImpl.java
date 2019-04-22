fa52ae1f12eeecdf2a170dc5122431a00a76f7c2
Leandro Carrasco <carrascoleandro@gmail.com>
2019-04-22 12:40:12 -0300

    Se configura Logback y se hace ejemplo de logueo

--- src/main/java/com/example/demorest/services/implementation/CountryServiceImpl.java
+++ src/main/java/com/example/demorest/services/implementation/CountryServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demorest.services.implementation;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,21 @@ import com.example.demorest.services.CountryService;
 @Service
 public class CountryServiceImpl implements CountryService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CountryServiceImpl.class);
+	
 	@Autowired
 	private CountryRepository countryRepository;
 	
 	@Override
 	public void createCountry(Country country) {
+		LOGGER.info("Saving country: " + country.getCode());
 		countryRepository.save(country);
+		LOGGER.info("Saved country: " + country.getCode());
 	}
 
 	@Override
 	public Country getCountry(String code) {
+		LOGGER.info("getCountry country: " + code);
 		List<Country> listCountry = countryRepository.findByCode(code);
 		Optional<Country> optionalCountry = listCountry.stream().findFirst();
 		return optionalCountry.orElse(null);
